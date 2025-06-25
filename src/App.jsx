0bf49672fb9b481d90b36bbcfbd57b3c4dd19d39
Hannah Capell <hannah.capell@gmail.com>
2025-06-25 12:01:43 +0200

    add new color on top of the list and card container for colors

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,25 @@ import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
 import "./App.css";
 import ColorForm from "./Components/ColorForm/ColorForm.jsx";
-import {uid} from 'uid';
+import { uid } from "uid";
 
 function App() {
   const [colors, setColors] = useState(initialColors);
   function handleAddColor(newColor) {
     const newColorWithId = { ...newColor, id: uid() };
     console.log(newColorWithId);
-    setColors((prevColors) => [...prevColors, newColorWithId]);
+    setColors((prevColors) => [newColorWithId, ...prevColors]);
   }
 
   return (
     <>
       <h1>Theme Creator</h1>
       <ColorForm onSubmitColor={handleAddColor} />
-      {colors.map((color) => {
-        return <Color key={color.id} color={color} />;
-      })}
+      <div className="card-container">
+        {colors.map((color) => {
+          return <Color key={color.id} color={color} />;
+        })}
+      </div>
     </>
   );
 }
